8cbda5e4d4656e9ac4d84a5e97deed72ca06a106
dude <dude@dude.ru>
2023-02-20 19:45:02 +0300

    home_work_lessson_10_edit

--- home_work_lesson_9/bot_commands.py
+++ home_work_lesson_9/bot_commands.py
@@ -1,7 +1,14 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import datetime
+import requests
+import telegram
+
+
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bs4 import BeautifulSoup as b
+
 from spy import *
+
 
 async def aloxa(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
@@ -9,10 +16,11 @@ async def aloxa(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
-    await update.message.reply_text(f'/hello\n/time\n/sum\n/help')
+    await update.message.reply_text(f'/hello\n/time\n/sum\n/help\n/moon\n/newyear\n/joke')
 
 async def time_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     log(update, context)
+    
     await update.message.reply_text(f'{datetime.datetime.now().time()}')
 
 async def sum_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -22,4 +30,23 @@ async def sum_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     items = msg.split() # sum 123 56789
     x = int(items[1])
     y = int(items[2])
-    await update.message.reply_text(f"{x} + {y} = {x + y}")
+    await update.message.reply_text(f"{x} + {y} = {x + y}", reply_markup=mar)
+
+async def phase_moon(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    data = datetime.datetime.now().date().isoformat()
+    day = data.split("-")[2]
+    ph_moon = ("Растущая луна", "Полнолуние", "Убывающая луна", "Последняя четверть", 
+               "Старая луна", "Новолуние", "Молодая луна", "Первая четверть")
+    await update.message.reply_text(f'Фаза луны - "{ph_moon[int(float(day)//3.6913)]}"')
+
+async def new_year(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    day_year = 365 - int(datetime.datetime.now().strftime("%j"))
+    await update.message.reply_text(f'До нового года осталось {day_year} дней')
+
+async def joke(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = "https://www.anekdot.ru/random/anekdot/"
+    read = requests.get(url)
+    soup = b(read.text, 'html.parser')
+    s = soup.find('div', class_="text")
+    await update.message.reply_text(f"{s.text}")
+    
--- home_work_lesson_9/main.py
+++ home_work_lesson_9/main.py
@@ -1,47 +1,7 @@
-# from isOdd import isOdd
-
-
-# print(isOdd(0)) 
-# print(isOdd(4))
-# print(isOdd(10)) 
-# print(isOdd(43))
-
-# import emoji
-
-# print(emoji.emojize('Python is :thumbs_up:'))
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-# # Fixing random state for reproducibility
-# np.random.seed(19680801)
-
-
-# plt.rcdefaults()
-# fig, ax = plt.subplots()
-
-# # Example data
-# people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-# y_pos = np.arange(len(people))
-# performance = 3 + 10 * np.random.rand(len(people))
-# error = np.random.rand(len(people))
-
-# ax.barh(y_pos, performance, xerr=error, align='center')
-# ax.set_yticks(y_pos, labels=people)
-# ax.invert_yaxis()  # labels read top-to-bottom
-# ax.set_xlabel('Performance')
-# ax.set_title('How fast do you want to go today?')
-
-# plt.show()
-
-# list = [1,2,3,2,7,10]
-# plt.plot(list)
-
-# plt.show()
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
+
 
 app = ApplicationBuilder().token("5893227796:AAHj9t6W1epzcjmNCBn_4zpx0YuOIDAViLg").build()
 
@@ -49,5 +9,8 @@ app.add_handler(CommandHandler("hello", aloxa))
 app.add_handler(CommandHandler("time", time_cmd))
 app.add_handler(CommandHandler("help", help_cmd))
 app.add_handler(CommandHandler("sum", sum_cmd))
+app.add_handler(CommandHandler("moon", phase_moon))
+app.add_handler(CommandHandler("newyear", new_year))
+app.add_handler(CommandHandler("joke", joke))
 print("server started")
 app.run_polling()
